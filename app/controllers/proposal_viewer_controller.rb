3228a536c084623ce8c62fcf9cde91dba9daf6ce
HeeL <parizhskiy@gmail.com>
2012-12-02 21:16:48 +0100

    added partial support

--- app/controllers/proposal_viewer_controller.rb
+++ app/controllers/proposal_viewer_controller.rb
@@ -3,8 +3,17 @@ class ProposalViewerController < ApplicationController
   def show
     proposal = Proposal.find(params[:id])
     client = proposal.client
-    section = ProposalSection.find_by_proposal_id(params[:id])
+    proposal_sections = []
+    proposal.proposal_sections.each do |section|
+      section_values = {
+        :section_header => section.name,
+        :section_content => section.description
+      }
+      proposal_sections.push(section_values)
+    end
+
     template = Template.new('/public/proposal-template/index.html')
+    
     variables = {
       :client_name => client.name,
       :client_website => client.website,
@@ -12,9 +21,11 @@ class ProposalViewerController < ApplicationController
       :proposal_name => proposal.name,
       :proposal_send_date => proposal.send_date,
       :proposal_user_name => proposal.user_name,
-      :section_header => section.name,
-      :section_content => section.description
+      :partial => {
+        'proposal-content' => proposal_sections
+      }
     }
+    
     template.add_variables(variables)
     render :text => template.render
   end
--- app/models/template.rb
+++ app/models/template.rb
@@ -1,3 +1,5 @@
+require 'nokogiri'
+
 class Template
 
   ROOT_PATH = "#{Rails.root}"
@@ -12,9 +14,44 @@ class Template
   end
 
   def render
-    output = File.read("#{ROOT_PATH}#{@path}")
-    @variables.each{|variable, value| output.gsub!("{#{variable.to_s}}", value.to_s)}
-    output
+    @output = File.read("#{ROOT_PATH}#{@path}")
+    replace_variables
+    @output
+  end
+
+  private 
+
+  def replace_variables
+    @variables.each do |variable, value|
+      if variable == :partial
+        replace_partial(value)
+      else
+        @output.gsub!("{#{variable.to_s}}", value.to_s)
+      end
+    end
+  end
+
+  def replace_partial(partial_arr)
+    partial_arr.each do |element_id, variables|
+      partial = get_partial(element_id)
+      partial_output = ''
+      variables.each do |variable|
+        partial_content = ''
+        variable.each do |key, value|
+          if(partial_content.empty?)         
+            partial_content = partial.gsub("{#{key}}", value)
+          else
+            partial_content = partial_content.gsub("{#{key}}", value)
+          end
+        end
+        partial_output += partial_content
+      end
+      @output.gsub!(partial, partial_output)
+    end
+  end
+
+  def get_partial(element_id)
+    Nokogiri::HTML(@output).xpath("//div[@id='#{element_id}']").inner_html
   end
 
 end
